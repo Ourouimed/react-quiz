e4b1f5934df6f2c87000c9e059585bef58bb1cf4
Ourouimed <medtube835@gmail.com>
2025-01-29 10:31:07 +0000

    fix a small deploy issue

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/reac-quiz/',
+  base: '/react-quiz/',
   plugins: [react() , tailwindcss()],
 })
